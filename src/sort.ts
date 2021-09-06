75a79ef495f26e9710c734bc0030ac99dcd59dd2
fenghan <fenghan770@gmail.com>
2021-09-06 16:58:46 +0800

    :sparkles: add mergeSort and quickSort algorithm

--- src/sort.ts
+++ src/sort.ts
@@ -86,3 +86,127 @@ export const insertionSort = <T>(
 
   return array
 }
+
+/**
+ * 归并排序 时间复杂度 O(nlog(n)) 空间复杂度 O(n)
+ * @param 排序数组
+ * @param compareFn 比较函数
+ */
+export const mergeSort = <T>(
+  array: T[],
+  compareFn: CompareFn<T> = defaultCompare
+): T[] => {
+  if (array.length <= 1) return array
+
+  // 分治法
+  // 拆分
+  const middleIndex = ~~(array.length / 2)
+  const left = mergeSort(array.slice(0, middleIndex), compareFn)
+  const right = mergeSort(array.slice(middleIndex), compareFn)
+
+  // 合并
+  return merge(left, right, compareFn)
+}
+
+/**
+ * 归并操作 将两个有序数组进行有序合并
+ */
+const merge = <T>(left: T[], right: T[], compareFn: CompareFn<T>): T[] => {
+  const result: T[] = []
+  let i = 0
+  let j = 0
+
+  while (i < left.length && j < right.length) {
+    const temp =
+      compareFn(left[i], right[j]) === Compare.LESS_THAN
+        ? left.shift()
+        : right.shift()
+
+    result.push(temp as T)
+  }
+
+  return result.concat(left, right)
+}
+
+/**
+ * 快速排序 时间复杂度 O(nlog(n)) 空间复杂度
+ * @param 排序数组
+ * @param compareFn 比较函数
+ */
+export const quickSort = <T>(
+  array: T[],
+  compareFn: CompareFn<T> = defaultCompare
+): T[] => {
+  return quick(array, 0, array.length - 1, compareFn)
+}
+
+/**
+ * 快速排序
+ * @param array 待排序数组
+ * @param left 左指针
+ * @param right 右指针
+ * @param compareFn 比较函数
+ */
+const quick = <T>(
+  array: T[],
+  left: number,
+  right: number,
+  compareFn: CompareFn<T>
+) => {
+  let index
+  if (array.length > 1) {
+    // 拆分位置
+    index = partition(array, left, right, compareFn)
+
+    if (left < index - 1) {
+      // 比基准值小的值组成的新数组
+      quick(array, left, index - 1, compareFn)
+    }
+
+    if (right > index) {
+      // 比基准值大的值组成的新数组
+      quick(array, index, right, compareFn)
+    }
+  }
+
+  return array
+}
+
+/**
+ * 划分操作
+ * @param array
+ * @param left
+ * @param right
+ * @param compareFn
+ */
+const partition = <T>(
+  array: T[],
+  left: number,
+  right: number,
+  compareFn: CompareFn<T>
+): number => {
+  // 选择基准值，这里是中间的值
+  const pivot = array[~~((left + right) / 2)]
+
+  let i = left
+  let j = right
+
+  while (i <= j) {
+    while (compareFn(array[i], pivot) === Compare.LESS_THAN) {
+      i++
+    }
+
+    while (compareFn(array[j], pivot) === Compare.BIGGER_THAN) {
+      j--
+    }
+
+    if (i < j) {
+      swap(array, i++, j--)
+    } else if (i === j) {
+      i++
+      j--
+    }
+  }
+
+  return i
+}
