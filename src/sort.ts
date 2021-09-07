3e473ff737949d6989ac3adaa8147d981ad70d66
fenghan <fenghan770@gmail.com>
2021-09-07 11:22:22 +0800

    :sparkles: add countingSort and bucketSort algorithm

--- src/sort.ts
+++ src/sort.ts
@@ -210,3 +210,70 @@ const partition = <T>(
 
   return i
 }
+
+/**
+ * 计数排序 时间复杂度 O(n + k) 空间复杂度 O(n + k)
+ * @param array 待排序整数数组
+ */
+export const countingSort = (array: number[]): number[] => {
+  if (array.length < 2) return array
+
+  const counts = []
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i]
+    counts[element] >= 1 ? counts[element]++ : (counts[element] = 1)
+  }
+
+  let index = 0
+  for (let j = 0; j < counts.length; j++) {
+    let count = counts[j]
+
+    while (count > 0) {
+      array[index++] = j
+      count--
+    }
+  }
+
+  return array
+}
+
+/**
+ * 桶排序 平均时间复杂度 O(n + k)  空间复杂度 O(n * k)
+ * @param array 待排序整数数组
+ * @param num 桶数量
+ */
+export const bucketSort = (array: number[], num: number) => {
+  const max = Math.max(...array)
+  const min = Math.min(...array)
+
+  const buckets: number[][] = []
+  // 每个桶大小
+  const bucketsSize = Math.floor((max - min) / num) + 1
+
+  for (let i = 0; i < array.length; i++) {
+    const bucketIndex = ~~(array[i] / bucketsSize)
+
+    if (!buckets[bucketIndex]) {
+      buckets[bucketIndex] = []
+    }
+
+    const bucket = buckets[bucketIndex]
+    bucket.push(array[i])
+
+    // 每次插入后排序
+    let l = bucket.length - 1
+    while (l > 0) {
+      if (bucket[l] < bucket[l - 1]) {
+        swap(bucket, l, l - 1)
+      }
+      l--
+    }
+  }
+
+  const sortedArray = []
+  for (let i = 0; i < buckets.length; i++) {
+    buckets[i] && sortedArray.push(...buckets[i])
+  }
+
+  return sortedArray
+}
