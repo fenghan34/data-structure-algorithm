d1686b8b7d3b1834571a0b8e11e93ba35cc58b96
fenghan <fenghan770@gmail.com>
2021-09-24 17:51:55 +0800

    :white_check_mark: test MaxHeap

---
+++ tests/data-structures/heap/max-heap.spec.ts
@@ -0,0 +1,17 @@
+import { MaxHeap } from '@/data-structures/heap/max-heap'
+
+describe('MaxHeap', () => {
+  let heap: MaxHeap<number>
+
+  beforeEach(() => {
+    heap = new MaxHeap<number>()
+  })
+
+  test('return maximum value', () => {
+    expect(heap.findMaximum()).toBeUndefined()
+    expect(heap.insert(1)).toBeTruthy()
+    expect(heap.insert(2)).toBeTruthy()
+    expect(heap.insert(3)).toBeTruthy()
+    expect(heap.findMaximum()).toBe(3)
+  })
+})
